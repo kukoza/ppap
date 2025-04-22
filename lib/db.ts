618dc37f82fc917eed0d89a6e8f8d828825c5513
kukoza <gglike21@gmail.com>
2025-04-22 15:34:56 +0700

    ไกไก

--- lib/db.ts
+++ lib/db.ts
@@ -3,8 +3,8 @@ import mysql from "mysql2/promise"
 // กำหนดค่าการเชื่อมต่อ
 const dbConfig = {
   host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "ahtijithongkittikul",
-  password: process.env.DB_PASSWORD || "Gc$k7636m",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "carbookingsystem",
   waitForConnections: true,
   connectionLimit: 10,
